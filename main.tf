3c102fd77a9e6f12ca36dd397fadfa68c06b139c
Christopher Ekong <christopher.ekong@gmail.com>
2025-12-17 17:52:15 +0100

    provisioning infra

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "security_group" {
   source = "./modules/security-group"
 
   vpc_id            = module.vpc.vpc_id
-  allowed_ssh_cidr  = var.allowed_ssh_cidr
+ allowed_ssh_cidr = [var.allowed_ssh_cidr]
   tags              = var.tags
 }
 
