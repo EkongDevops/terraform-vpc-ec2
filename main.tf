f3417b804ea720a156172020f4dc56ff301ff74f
Christopher Ekong <christopher.ekong@gmail.com>
2025-12-17 17:37:10 +0100

    provisioning infra

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "s3" {
-    bucket = "vpcec22"
-    key    = "vpc-ec2/terraform.tfstate"
-    region = "eu-north-1"
-  }
-
+ 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
