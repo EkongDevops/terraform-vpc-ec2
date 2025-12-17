27ab81aeb3f802472f9efbd389dafa530d2b38de
Christopher Ekong <christopher.ekong@gmail.com>
2025-12-17 17:44:07 +0100

    provisioning infra

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "vpcec22"
     key     = "vpc-ec2/terraform.tfstate"
-    region  = "var.aws_region"
+    region  = "eu-north-1"
     encrypt = true
   }
 }
